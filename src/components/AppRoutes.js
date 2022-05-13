81cfdf776f9c47cd1a32dec6dd19ebd535eea454
brewchetta <chett.tiller@gmail.com>
2022-05-13 09:30:14 -0400

    proper components added to routes

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -11,13 +11,13 @@ function AppRoutes(props) {
   // RENDER //
   return (
     <Routes>
-      <Route path='/' element={<div>Home Route Goes Here</div>} />
+      <Route path='/' element={<div>Home Component</div>} />
       {/* Route for index of terms */}
-      <Route path='/dictionary' element={<div>Dictionary Route Goes Here</div>} />
+      <Route path='/dictionary' element={<Dictionary />} />
       {/* Route for creating an event */}
-      <Route path='/create-event' element={<div>Create Event Route Goes Here</div>} />
+      <Route path='/create-event' element={<EventCreator />} />
       {/* Route for creating a character (creates the character and the event that makes them) */}
-      <Route path='/create-character' element={<div>Create Character Route Goes Here</div>} />
+      <Route path='/create-character' element={<CharacterCreator />} />
     </Routes>
   )
 
