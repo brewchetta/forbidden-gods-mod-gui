1157325e061bcbc110bd6825c41e4ab181b3f956
brewchetta <chett.tiller@gmail.com>
2022-05-17 15:50:59 -0400

    finished terms and dictionary items!

--- src/data/_effects.js
+++ src/data/_effects.js
@@ -5,7 +5,17 @@ const effects = [
     recommended: 'filename appended with mod initials'
   },
   {
+    name: 'SHOW_RANDOM_EVENT_WITH_TERM_IN_ID',
+    description: 'Show another event of the type INERT with a specific term in its name / id',
+    recommended: 'a search term'
+  },
+  {
     name: 'GAIN_POWER',
+    description: 'Give the god points of power. Negative points will reduce the god power.',
+    recommended: 'number between 9 and -9'
+  },
+  {
+    name: 'ADD_POWER',
     description: 'Give the god points of power. Negative points will reduce the god power.',
     recommended: 'number between 9 and -9'
   },
@@ -18,6 +28,21 @@ const effects = [
     name: 'LOCATION_POP_MULT',
     description: 'Multiplies the location population, 100 remains the same and lower reduces.',
     recommended: 'number between 0 and 200'
+  },
+  {
+    name: 'DESTROY_LOCATION',
+    description: 'Immediately razes the location.',
+    recommended: 'none'
+  },
+  {
+    name: 'OPHANIM_TAKE_OVER',
+    description: 'Immediately causes Ophanim to take over the location.',
+    recommended: 'none'
+  },
+  {
+    name: 'INCREASE_UNREST',
+    description: 'Increases unrest in a location by a specified amount. Can be negative.',
+    recommended: 'number between -200 and 200'
   },
   {
     name: 'ADD_TEMPORARY_WORLD_PANIC',
@@ -54,6 +79,81 @@ const effects = [
   {
     name: 'PAN_TO',
     description: 'Pans to the LOCATION where the event occurs.',
+    recommended: 'none'
+  },
+  {
+    name: 'BEGIN_HIDING',
+    description: 'Makes the UNIT begin hiding (unclear what this means).',
+    recommended: 'none'
+  },
+  {
+    name: 'CANCEL_ALL_ATTACKS',
+    description: 'Makes the UNIT cancel their combats.',
+    recommended: 'none'
+  },
+  {
+    name: 'ATTACKER_ADD_PROFILE',
+    description: 'Adds profile to the attacking UNIT. Can be negative.',
+    recommended: 'a number between -20 and 20'
+  },
+  {
+    name: 'DEFENDER_ADD_PROFILE',
+    description: 'Adds profile to the defending UNIT. Can be negative.',
+    recommended: 'a number between -20 and 20'
+  },
+  {
+    name: 'ATTACKER_ADD_MENACE',
+    description: 'Adds menace to the attacking UNIT. Can be negative.',
+    recommended: 'a number between -20 and 20'
+  },
+  {
+    name: 'DEFENDER_ADD_MENACE',
+    description: 'Adds menace to the defending UNIT. Can be negative.',
+    recommended: 'a number between -20 and 20'
+  },
+  {
+    name: 'DEFENDER_GAINS_TRAIT',
+    description: 'Adds a trait to the defending UNIT.',
+    recommended: 'name of the trait'
+  },
+  {
+    name: 'ATTACKER_GAINS_TRAIT',
+    description: 'Adds a trait to the defending UNIT.',
+    recommended: 'name of the trait'
+  },
+  {
+    name: 'DEFENDER_HATES_ATTACKER',
+    description: 'Adds a trait to the defending UNIT to make them hate the attacking unit.',
+    recommended: 'none'
+  },
+  {
+    name: 'ATTACKER_HATES_DEFENDER',
+    description: 'Adds a trait to the attacking UNIT to make them hate the defending unit.',
+    recommended: 'none'
+  },
+  {
+    name: 'ATTACKER_GAINS_PREFERENCE_FOR_TAG',
+    description: 'The attacking UNIT gains a like of the specified tag.',
+    recommended: 'A tag name'
+  },
+  {
+    name: 'ATTACKER_LOSE_PREFERENCE_FOR_TAG',
+    description: 'The attacking UNIT gains a dislike of the specified tag.',
+    recommended: 'A tag name'
+  },
+  {
+    name: 'DEFENDER_GAINS_PREFERENCE_FOR_TAG',
+    description: 'The defending UNIT gains a like of the specified tag.',
+    recommended: 'A tag name'
+  },
+  {
+    name: 'DEFENDER_LOSE_PREFERENCE_FOR_TAG',
+    description: 'The defending UNIT gains a dislike of the specified tag.',
+    recommended: 'A tag name'
+  },
+  {
+    name: 'RANDOM_TELEPORT_TO_LAND',
+    description: 'Makes the UNIT randomly teleport.',
     recommended: 'none'
   },
   {
@@ -108,6 +208,16 @@ const effects = [
     recommended: 'none'
   },
   {
+    name: 'INCREASE_P1_TO_P2_RELATION_LIMITED',
+    description: 'Makes PERSON 1 love PERSON 2.',
+    recommended: 'the amount of love you want'
+  },
+  {
+    name: 'INCREASE_P2_TO_P1_RELATION_LIMITED',
+    description: 'Makes PERSON 2 love PERSON 1.',
+    recommended: 'the amount of love you want'
+  },
+  {
     name: 'DECREASE_P1_TO_P2_RELATION',
     description: 'Makes PERSON 1 hate PERSON 2.',
     recommended: 'none'
@@ -118,13 +228,43 @@ const effects = [
     recommended: 'none'
   },
   {
+    name: 'XENOPHOBIA',
+    description: 'Increases xenophobia between PERSON 1 and PERSON 2 (what DOES THIS MEAN?).',
+    recommended: 'number between -1 and 1'
+  },
+  {
+    name: 'VENDETTA',
+    description: 'Increases vendetta between PERSON 1 and PERSON 2 (what DOES THIS MEAN?).',
+    recommended: 'number between -1 and 1'
+  },
+  {
     name: 'CITY_REBELS',
     description: 'Triggers a LOCATION to rebel.',
     recommended: 'none'
   },
   {
+    name: 'KILL_PERSON',
+    description: 'Kill the PERSON.',
+    recommended: 'none'
+  },
+  {
+    name: 'MOURNING_PROLONGED',
+    description: 'Prolongs the mourning period for the PERSON by a number of turns.',
+    recommended: 'Number between 1 and 100'
+  },
+  {
     name: 'RULER_DIES',
     description: 'Kill the ruler of a LOCATION.',
+    recommended: 'none'
+  },
+  {
+    name: 'ABDICATE',
+    description: 'The ruler of a LOCATION abdicates their seat of power.',
+    recommended: 'none'
+  },
+  {
+    name: 'BECOME_HERO',
+    description: 'The ruler of a LOCATION becomes a hero.',
     recommended: 'none'
   },
   {
@@ -233,6 +373,11 @@ const effects = [
     recommended: 'True or False'
   },
   {
+    name: 'CACHE_GOLD',
+    description: 'Creates a cache of gold at the LOCATION.',
+    recommended: 'True or False'
+  },
+  {
     name: 'MAKE_FEMALE',
     description: 'Makes the PERSON use she/her pronouns.',
     recommended: 'none'
@@ -269,23 +414,18 @@ const effects = [
   },
   {
     name: 'OTHER_GAIN_MENACE',
-    description: 'Adds menace to PERSON 2. Can be negative.',
+    description: 'Adds menace to the OTHER in a P2P. Can be negative.',
     recommended: 'Number between -20 and 20'
   },
   {
-    name: 'CHANGE_PLAGUE',
-    description: 'Changes a LOCATION plague modifier to a certain level.',
-    recommended: 'Number between 0 and 200'
+    name: 'OTHER_LOSE_SANITY',
+    description: 'Loses sanity from the OTHER in a P2P. Can be negative.',
+    recommended: 'Number between -20 and 20'
   },
   {
-    name: 'CHANGE_DEVASTATION',
-    description: 'Changes a LOCATION devastation modifier to a certain level.',
-    recommended: 'Number between 0 and 200'
-  },
-  {
-    name: 'CHANGE_POLITICAL_AGITATION',
-    description: 'Changes a LOCATION political agitation modifier to a certain level.',
-    recommended: 'Number between 0 and 200'
+    name: 'OTHER_GAIN_SHADOW',
+    description: 'Adds shadow to the OTHER in a P2P. Can be negative.',
+    recommended: 'Number between -100 and 100'
   },
   {
     name: 'CHANGE_POLITICAL_INSTABILITY',
@@ -317,7 +457,51 @@ const effects = [
     description: 'Changes a LOCATION unrest modifier to a certain level.',
     recommended: 'Number between 0 and 200'
   },
-  // TODO: Add other modifiers
+  {
+    name: 'CHANGE_PLAGUE',
+    description: 'Changes a LOCATION plague modifier to a certain level.',
+    recommended: 'Number between 0 and 200'
+  },
+  {
+    name: 'CHANGE_PLAGUE_IMMUNITY',
+    description: 'Changes a LOCATION plague immunity modifier to a certain level.',
+    recommended: 'Number between 0 and 200'
+  },
+  {
+    name: 'CHANGE_DEVASTATION',
+    description: 'Changes a LOCATION devastation modifier to a certain level.',
+    recommended: 'Number between 0 and 200'
+  },
+  {
+    name: 'CHANGE_POLITICAL_AGITATION',
+    description: 'Changes a LOCATION devastation modifier to a certain level.',
+    recommended: 'Number between 0 and 200'
+  },
+  {
+    name: 'CHANGE_WARD',
+    description: 'Changes a LOCATION ward modifier to a certain level.',
+    recommended: 'Number between 0 and 200'
+  },
+  {
+    name: 'CHANGE_WARD',
+    description: 'Changes a LOCATION ward modifier to a certain level.',
+    recommended: 'Number between 0 and 200'
+  },
+  {
+    name: 'CHANGE_ARCANE_FORTRESS',
+    description: 'Changes a LOCATION arcane fortress modifier to a certain level.',
+    recommended: 'Number between 0 and 200'
+  },
+  {
+    name: 'CHANGE_GEOMANCY',
+    description: 'Changes a LOCATION geomancy modifier to a certain level.',
+    recommended: 'Number between 0 and 200'
+  },
+  {
+    name: 'PLACE_ARCANE_SECRET',
+    description: 'Adds an arcane secret modifier to a LOCATION.',
+    recommended: 'none'
+  },
   {
     name: 'CREATE_HERO_WARRIOR',
     description: 'Creates a new HERO at LOCATION.',
